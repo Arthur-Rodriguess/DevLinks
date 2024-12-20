f5b9fa1ee961dc1c7cca0aeb59047cfd42dfe693
Arthur-Rodriguess <artrod150309@gmail.com>
2024-12-20 20:44:28 -0300

    modified a lot of things

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/assets/avatar-light.png")
+    img.setAttribute("src", "./imgs/foto-arthur-light.png")
     img.setAttribute("alt", "Foto do Mayk Brito sorrindo com óculos escuro e casaco"
     )
   } else {
-    img.setAttribute("src", "./assets/assets/avatar.png")
+    img.setAttribute("src", "./imgs/foto-arthur.png")
     img.setAttribute("alt", "Foto do Mayk Brito sorrindo com um fundo amarelo")
   }
 }
